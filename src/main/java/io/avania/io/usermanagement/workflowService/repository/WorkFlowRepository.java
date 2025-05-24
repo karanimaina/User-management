f89336c5b9f84af6a2a8b7d94634ec0afdfb571a
Felix Maina <maina.felix@eclectics.io>
2025-05-24 22:56:39 +0300

    update WorkFlowRepository.java

--- src/main/java/io/avania/io/usermanagement/workflowService/repository/WorkFlowRepository.java
+++ src/main/java/io/avania/io/usermanagement/workflowService/repository/WorkFlowRepository.java
@@ -1,6 +1,6 @@
 package io.avania.io.usermanagement.workflowService.repository;
 
-import com.eclectics.io.usermodule.workflowService.model.WorkFlow;
+import io.avania.io.usermanagement.workflowService.model.WorkFlow;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
