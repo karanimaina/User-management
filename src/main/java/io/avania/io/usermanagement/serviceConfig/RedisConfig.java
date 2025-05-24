eca64b6a5baab3e90656b3c3c4f09e9d98df2548
Felix Maina <maina.felix@eclectics.io>
2025-05-24 22:48:55 +0300

    update  RedisConfig.java

--- src/main/java/io/avania/io/usermanagement/serviceConfig/RedisConfig.java
+++ src/main/java/io/avania/io/usermanagement/serviceConfig/RedisConfig.java
@@ -9,10 +9,10 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${redis.port}")
     private int redisPort;
 
     @Bean
