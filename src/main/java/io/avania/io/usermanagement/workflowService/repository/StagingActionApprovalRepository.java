49acba11e507eb40fa5dc25d28ee8f534db295ef
Felix Maina <maina.felix@eclectics.io>
2025-05-24 22:52:40 +0300

    update  StagingActionApprovalRepository.java

--- src/main/java/io/avania/io/usermanagement/workflowService/repository/StagingActionApprovalRepository.java
+++ src/main/java/io/avania/io/usermanagement/workflowService/repository/StagingActionApprovalRepository.java
@@ -1,6 +1,6 @@
 package io.avania.io.usermanagement.workflowService.repository;
 
-import com.eclectics.io.usermodule.workflowService.model.StagingActionApproval;
+import io.avania.io.usermanagement.workflowService.model.StagingActionApproval;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StagingActionApprovalRepository extends JpaRepository<StagingActionApproval,Long> {
